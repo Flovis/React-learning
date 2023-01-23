77992e61d35d3c0ee2a9c1ce5dcd005ec2ae943b
Flovis <floviskabandafkm@gmail.com>
2023-01-23 16:25:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+    //state
+const [voitures, setVoitures] = useState([
+    {id:1, nom:'tesla'},
+    {id:1, nom:'odi'},
+    {id:3, nom:'toyata'},
+    {id:1, nom:'bmw'},
+
+]);
+
+//fonction
+
+
+//render
+return (
+    <div>
+        <h1>Voiture</h1>
+        <ul>
+            {voitures.map((fruit) =>{
+                if(fruit.id === 1)
+                    return <li>{fruit.nom} </li>
+            })}
+        </ul>
     </div>
-  );
+);
+// render
 }
 
-export default App;
+export default App; 
+
